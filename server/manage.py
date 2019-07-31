8fd11d7bea4f6ae6c25b64c9f6377072a8368ee9
Yuhang Li <18326007+WilliamYuhangLee@users.noreply.github.com>
2019-07-31 18:45:44 -0400

    Add cli tool for initializing and dropping database

--- server/manage.py
+++ server/manage.py
@@ -1,4 +1,4 @@
-from flask_script import Shell, Manager
+from flask_script import Shell, Manager, prompt_bool, prompt
 from flask_migrate import MigrateCommand
 
 from app import create_app, db
@@ -16,6 +16,17 @@ def _make_context():
 
 manager.add_command("shell", Shell(make_context=_make_context))
 
+
+@manager.command
+def initdb():
+    db.create_all()
+    print("All tables have been created.")
+
+
+@manager.command
+def dropdb():
+    if prompt_bool("Are you sure you want to lose all your data"):
+        db.drop_all()
 
 if __name__ == "__main__":
     manager.run()
