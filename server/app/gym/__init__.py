d6a0c70ab7f7cde901aee548c91fa5020ac82e35
Yuhang Li <18326007+WilliamYuhangLee@users.noreply.github.com>
2019-07-31 15:10:09 -0400

    Add example json response to endpoint stats

--- server/app/gym/__init__.py
+++ server/app/gym/__init__.py
@@ -1,8 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api
-from .resources import Todo
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
-api.add_resource(Todo, '/')
+from . import resources
--- server/app/gym/resources.py
+++ server/app/gym/resources.py
@@ -1,8 +1,27 @@
+from flask import jsonify
 from flask_restful import Resource
 from flask_restful import current_app as app
+from . import api
 
 
-class Todo(Resource):
+class Stats(Resource):
 
-    def get(self):
-        return app.config['SQLALCHEMY_DATABASE_URI']
+    def get(self, timeframe):
+        example = {
+            'message': "Success.",
+            'status': 200,
+            'data': {
+                0: 10,
+                1: 20,
+                2: 20,
+                3: 30,
+                4: 40,
+                5: 50,
+                6: 60,
+            }
+        }
+        if timeframe == 'week':
+            return jsonify(example)
+
+
+api.add_resource(Stats, '/stats/<string:timeframe>')
